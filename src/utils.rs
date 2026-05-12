35d52cbdbfaf1cc768466645e3aef4d1bab8c1da
bscout9956 <bs.blackscout.bs@gmail.com>
2026-05-12 00:19:02 -0300

    Implement some useful trait stuff

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,15 @@
 pub trait ByteUtils {
     fn to_lossy_string(&self) -> String;
+
+    fn from_be_to_u16(&self) -> u16;
 }
 
 impl ByteUtils for [u8] {
     fn to_lossy_string(&self) -> String {
         String::from_utf8_lossy(self).into_owned()
+    }
+    fn from_be_to_u16(&self) -> u16 {
+        let bytes: [u8; 2] = self.try_into().expect("Slice with incorrect length");
+        return u16::from_be_bytes(bytes);
     }
 }
